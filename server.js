d872a1f6ac7478c6cedb027ea474b1592819e98f
Michela <micheladellagatta1@gmail.com>
2025-12-11 12:37:02 +0100

    Authorize domain for webapp calls

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express = require("express");
 const moviesRouter = require("./routers/movies");
 const errorHandler = require("./middlewares/errorHandler");
 const notFound = require("./middlewares/notFound");
-
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/movies", moviesRouter);
